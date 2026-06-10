7e6c27109be0c828e7517a369da5c7b3d0ac7cb1
mark-uplaunch <mark@uplaunch.com.au>
2026-06-10 11:44:31 +0800

    Added the pug and route for custom object update

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(express.json());
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
 const PRIVATE_APP_ACCESS = '';
 
+
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 app.get('/', async (req, res) => {
@@ -35,7 +36,7 @@ app.get('/', async (req, res) => {
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 app.get('/update-cobj', async (req, res) => {
-    const vehicles = 'https://api.hubspot.com/crm/v3/objects/2-217083325';
+    const vehicles = `https://api.hubspot.com/crm/v3/objects/2-217083325?properties=name,type,brand`;
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
@@ -43,14 +44,46 @@ app.get('/update-cobj', async (req, res) => {
     try {
         const resp = await axios.get(vehicles, { headers });
         const data = resp.data.results;
-        res.render('vehicles', { title: 'Vehicles | HubSpot APIs', data });      
+        console.log(data);   
+        res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum', data });  
     } catch (error) {
         console.error(error);
     }
+
+    
 });
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
+app.post('/update-cobj', async (req, res) => {
+    const updates = req.body.vehicles.map(vehicle => ({
+        id: vehicle.id,
+        properties: {
+            name: vehicle.name,
+            brand: vehicle.brand,
+            type: vehicle.type
+        }
+    }));
+
+    const update_payload = JSON.stringify({
+        inputs: updates
+    })
+    
+    const updateVehicles = `https://api.hubapi.com/crm/v3/objects/2-217083325/batch/update`;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try { 
+        await axios.post(updateVehicles, update_payload, { headers } );
+        res.redirect('/');
+    } catch(err) {
+        console.error(err);
+    }
+
+
+});
 // * Code for Route 3 goes here
 
 /** 
